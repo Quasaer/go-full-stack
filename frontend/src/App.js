6405c363c43cf4e0d5e3b4448e650c22fd4a2d5c
Yongle Xu <yonglexu08@gmail.com>
2021-11-13 23:04:03 +0000

    Axios call to backend api
    React calling api ping message to display.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      ping: "Waiting"
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/ping')
+      .then((res, err) => {
+        if(res.status === 200) {
+          this.setState({
+            ping: res.data.message
+          })
+        }
+        else {
+          console.log(err);
+        }
+      })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.state.ping}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
